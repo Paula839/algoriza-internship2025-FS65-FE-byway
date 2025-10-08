b00bc7e9a27122c7fa00527285f183b327971e8b
Paula <PaulaEmad00@gmail.com>
2025-10-08 15:16:57 +0300

    d2

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom"; // only HashRouter
+import { HashRouter } from "react-router-dom"; // use HashRouter instead of BrowserRouter
 import App from "./App";
 import "./index.css";
 import { AuthProvider } from "./context/AuthContext";
