f4237507feb720a4a50642ca4707d8b3bfb6cbf6
Paula <PaulaEmad00@gmail.com>
2025-10-08 14:45:22 +0300

    another fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom"; // only HashRouter
 import App from "./App";
 import "./index.css";
 import { AuthProvider } from "./context/AuthContext";
 import { CartProvider } from "./context/CartContext";
-
 import { LoadingProvider } from "./context/LoadingContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <LoadingProvider>
-      <AuthProvider>
-        <CartProvider>
-          <App />
-        </CartProvider>
-      </AuthProvider>
+        <AuthProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </AuthProvider>
       </LoadingProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
